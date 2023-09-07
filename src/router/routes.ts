0a51e92cea1d14e67663dc6eb6d04760270e281f
mrheio <drg.ov00@gmail.com>
2023-09-07 09:49:54 +0300

    refactor: group routes by PUBLIC or PROTECTED

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,12 +1,17 @@
 const ROUTES = {
-    HOME: '/',
-    LOGIN: '/login',
-    REGISTER: '/register',
-    DOCS: {
-        COMPONENTS: '/docs/components',
+    PUBLIC: {
+        HOME: '/',
+        LOGIN: '/login',
+        REGISTER: '/register',
+        DOCS: {
+            COMPONENTS: '/docs/components',
+        },
+        POSTS: '/posts',
+        POST: (id: string | number) => `/posts/${id}`,
     },
-    POSTS: '/posts',
-    POST: (id: string | number) => `/posts/${id}`,
-} as const;
+    PROTECTED: {
+        PROFILE: '/profile',
+    },
+};
 
 export default ROUTES;
